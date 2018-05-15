0241d42116ebbbb0b6b6a6ba1093f4ae1ed02f70
Alessandro Nakamuta <alessandro.ufms@gmail.com>
2018-05-15 18:10:22 -0400

    Initial Nuke (https://github.com/kean/Nuke) integration

--- Source/Classes/View Controllers/AXPreviewingPhotosViewController.swift
+++ Source/Classes/View Controllers/AXPreviewingPhotosViewController.swift
@@ -12,7 +12,7 @@ import MobileCoreServices
 import FLAnimatedImage
 
 @objc open class AXPreviewingPhotosViewController: UIViewController, AXNetworkIntegrationDelegate {
-    
+
     /// The photos to display in the `PhotosPreviewingViewController`.
     @objc open var dataSource: AXPhotosDataSource = AXPhotosDataSource() {
         didSet {
@@ -20,43 +20,43 @@ import FLAnimatedImage
             if self.networkIntegration == nil {
                 return
             }
-            
+
             self.networkIntegration.cancelAllLoads()
             self.configure(with: dataSource.initialPhotoIndex)
         }
     }
-    
+
     /// The `NetworkIntegration` passed in at initialization. This object is used to fetch images asynchronously from a cache or URL.
     /// - Initialized by the end of `commonInit(dataSource:networkIntegration:)`.
     @objc public fileprivate(set) var networkIntegration: AXNetworkIntegrationProtocol!
-    
+
     var imageView: FLAnimatedImageView {
         get {
             return self.view as! FLAnimatedImageView
         }
     }
-    
+
     // MARK: - Initialization
     @objc public init(dataSource: AXPhotosDataSource) {
         super.init(nibName: nil, bundle: nil)
         self.commonInit(dataSource: dataSource)
     }
-    
+
     @objc public init(dataSource: AXPhotosDataSource,
                       networkIntegration: AXNetworkIntegrationProtocol) {
-    
+
         super.init(nibName: nil, bundle: nil)
         self.commonInit(dataSource: dataSource,
                         networkIntegration: networkIntegration)
     }
-    
+
     @objc public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     fileprivate func commonInit(dataSource: AXPhotosDataSource,
                                 networkIntegration: AXNetworkIntegrationProtocol? = nil) {
-        
+
         self.dataSource = dataSource
 
         var `networkIntegration` = networkIntegration
@@ -69,30 +69,32 @@ import FLAnimatedImage
             networkIntegration = AFNetworkingIntegration()
             #elseif USE_KINGFISHER
             networkIntegration = KingfisherIntegration()
+            #elseif USE_NUKE
+            networkIntegration = NukeIntegration()
             #elseif USE_DEFAULT
             networkIntegration = SimpleNetworkIntegration()
             #else
             fatalError("Must be using one of the network integration subspecs if no `AXNetworkIntegration` is provided.")
             #endif
         }
-        
+
         self.networkIntegration = networkIntegration
         self.networkIntegration.delegate = self
     }
-    
+
     open override func loadView() {
         self.view = FLAnimatedImageView()
     }
-    
+
     open override func viewDidLoad() {
         super.viewDidLoad()
         self.imageView.contentMode = .scaleAspectFit
         self.configure(with: self.dataSource.initialPhotoIndex)
     }
-    
+
     open override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        
+
         var newSize: CGSize
         if self.imageView.image == nil && self.imageView.animatedImage == nil {
             newSize = CGSize(width: self.imageView.frame.size.width,
@@ -100,18 +102,18 @@ import FLAnimatedImage
         } else {
             newSize = self.imageView.intrinsicContentSize
         }
-        
+
         self.preferredContentSize = newSize
     }
-    
+
     fileprivate func configure(with index: Int) {
         guard let photo = self.dataSource.photo(at: index) else {
             return
         }
-        
+
         self.networkIntegration.loadPhoto(photo)
     }
-    
+
     // MARK: - AXNetworkIntegrationDelegate
     public func networkIntegration(_ networkIntegration: AXNetworkIntegrationProtocol, loadDidFinishWith photo: AXPhotoProtocol) {
         if let animatedImage = photo.ax_animatedImage {
@@ -135,19 +137,19 @@ import FLAnimatedImage
             }
         }
     }
-    
+
     public func networkIntegration(_ networkIntegration: AXNetworkIntegrationProtocol, loadDidFailWith error: Error, for photo: AXPhotoProtocol) {
         guard photo.ax_loadingState != .loadingCancelled else {
             return
         }
-        
+
         photo.ax_loadingState = .loadingFailed
         photo.ax_error = error
     }
-    
+
     public func networkIntegration(_ networkIntegration: AXNetworkIntegrationProtocol, didUpdateLoadingProgress progress: CGFloat, for photo: AXPhotoProtocol) {
         photo.ax_progress = progress
     }
-    
+
 }
 #endif
